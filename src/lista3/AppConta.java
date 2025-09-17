934d83a306f27c555326c20fdfd7d05de3647dc9
Luís Henrique <luishfmendes226@gmail.com>
2025-09-17 13:55:36 -0300

    Retirada do Scanner

--- src/lista3/AppConta.java
+++ src/lista3/AppConta.java
@@ -1,13 +1,9 @@
 package lista3;
 
-import java.util.Scanner;
-
 public class AppConta {
         public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+
             Conta xp = new Conta("Luís Henrique Freita Mendes", 1010, 1600);
-
-
             xp.mostraMenu();
         }
 }
